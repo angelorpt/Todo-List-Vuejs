2b284cf03ecc5ca2dafe1078c30c2ea44a2c26f5
Angelo <angelorpt@gmail.com>
2020-02-17 13:25:30 -0400

    Adicionado Data e Inclusão do metodo para incluir uma nova atividade

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
     <div id="app">
         <div class="container grid-xs py-2">
-            hello world
+            <img src="@/assets/logo.png" alt="Logo" class="img-responsive img-logo">
+            <form @submit.prevent="addTodo(todo)">
+                <div class="input-group">
+                    <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo">
+                    <button class="btn btn-primary input-group-btn">Adicionar</button>
+                </div>
+                {{ todos }}
+            </form>
         </div>
     </div>
-</template>
+</template> 
 
 <script>
     export default {
         name: 'App',
-        components: {
-        }
+        data() {
+            return {
+                todos: [],
+                todo: {
+                    checked: false,
+                },
+            }
+        },
+        methods: {
+            addTodo(todo) {
+                todo.id = Date.now()
+                this.todos.push(todo);
+            },
+        },
     }
 </script>
+
+<style scoped>
+    .img-logo {
+        max-width: 100px;
+        margin: 0 auto;
+    }
+</style>
